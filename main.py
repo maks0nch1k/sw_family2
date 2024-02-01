07817eeb468c446666288f4d9053c2d6f4aaef09
maks0nch1k <maksimf647@gmail.com>
2024-02-01 22:27:12 +0300

    update redirects and hrefs

--- main.py
+++ main.py
@@ -12,5 +12,30 @@ def main_page():
     return render_template("index.html", title="Учебник-справочник по конструкторам R:ED")
 
 
+@app.route("/controllers")
+def controllers():
+    return render_template("base.html", title="Контроллеры конструкторов R:ED")
+
+
+@app.route("/performers")
+def performers():
+    return render_template("base.html", title="Исполнители конструкторов R:ED")
+
+
+@app.route("/sensors")
+def sensors():
+    return render_template("base.html", title="Датчики конструкторов R:ED")
+
+
+@app.route("/software")
+def software():
+    return render_template("base.html", title="Программирование R:ED | Драйвера и ПО")
+
+
+@app.route("/sets")
+def sets():
+    return render_template("base.html", title="Комплектации")
+
+
 if __name__ == "__main__":
     main()
